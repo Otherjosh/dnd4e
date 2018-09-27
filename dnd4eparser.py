e35c96a5b91c31f361aa646e17814d82d24d83d0
Joshua Potts <joshuapotts80@gmail.com>
2018-09-27 01:27:52 -0500

    Commit with pretty output Doesn't show the hit line for the non special skill

--- dnd4eparser.py
+++ dnd4eparser.py
@@ -1,16 +1,16 @@
 import re
-import shelve
 
 
-# Currently works for skills with or without special abilities, 10 or 9 lines.
-# Todo address skills with more than 1 line of hit text
+# TODO assign each line to what piece of data it is and then pass those variables to a skill creation class
+# maybe count number of lines and have a counter for the iteration to determine which line is which? feels sloppy but
+# prolly would work. skill text has 10 lines of info
 spellbook = []      # TODO save this skillbook as a file
+
 special = False
 specialregex = re.compile(r'^Special:')  # to check if any lines are a special effect
 skillinfo = []
 
 
-# Used as a data structure, could probably make a dictionary instead of a class object
 class Skill(object):
     def __init__(self, name, class_, flavor, category, action, weapon, special, target, attack, hit):
         self.name = name
@@ -25,25 +25,20 @@ class Skill(object):
         self.hit = hit
 
 
-# strips lines and removes empty lines
+# removes empty lines
 def striplines(lines_):
     for line in lines_:
-        line = line.strip()
-        try:
-            if len(line) == 0:
-                lines_.remove(line)
-        except ValueError:
-            pass
+        if len(line) == 0:
+            lines_.remove(line)
     return lines_
 
 
-# Checks if the next skill in dump has a special effect
 def checkspecial(lines_):
     i = 0
     while i < 9:
         try:
             if re.match(specialregex, lines_[i]):
-                print(lines_[i])
+                # print(lines_[i])
                 global special
                 special = True
         except IndexError:
@@ -51,15 +46,17 @@ def checkspecial(lines_):
         i += 1
 
 
-# Grabs data from dump file
 with open('dnd4edump.txt', 'r') as rf:
-    lines = striplines(rf.readlines())
+    lines = striplines(rf.read().splitlines())
+    # print('with loop')
     rf.close()
 
 
 while lines:
+    # print('while loop')
     checkspecial(lines)
     if special:
+        # print('special = True')
         while len(skillinfo) < 10:
             skillinfo.append(lines.pop(0))
             if len(skillinfo) == 10:
@@ -70,6 +67,7 @@ while lines:
         continue
 
     if not special:
+        # print('not special')
         while len(skillinfo) < 9:
             try:
                 line_ = lines.pop(0)
@@ -86,8 +84,7 @@ while lines:
                                  'No special effects',
                                  skillinfo[6],
                                  skillinfo[7],
-                                 skillinfo[8]
-                                 )
+                                 skillinfo[8],)
                 spellbook.append(newskill)
                 special = False
         skillinfo = []
@@ -96,11 +93,8 @@ while lines:
 
 def spells():
     i = 0
-    # spellfile = shelve.open('spellbook.txt')
-    # spellfile['spellbook'] = spellbook
-    # spellfile.close()
     while i < len(spellbook):      # this can be used to search spellbook for a specific skill by name
-        print('================' + '\n' +
+        print('================' +
               spellbook[i].name + '\n' +
               spellbook[i].class_ + '\n' +
               spellbook[i].flavor + '\n' +
@@ -113,11 +107,9 @@ def spells():
               spellbook[i].hit
         )
         i += 1
-
+    print(lines)
 
 spells()
-# TODO support for skills <10 lines long, skills without a special: only have 9 lines
-# interate through cleanlines, if  length = 10 (?) pass the list to skill() class and wipe cleanlines
-# then continue iterating putting lines into cleanlines until length is reached again
-# maybe make a big list of skills that can be searched by the name element of the skills
 
+
+# This commit doesn't show the Hit line for the final skill, but has correct looking output
